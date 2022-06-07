4167014c8761548ec46fbe3f774f0548271e8d87
EC2 Default User <ec2-user@ip-172-16-28-131.ec2.internal>
2022-06-07 05:59:22 +0000

    deploy complete

--- model/code/infer.py
+++ model/code/infer.py
@@ -26,8 +26,6 @@ from uie_predictor import UIEPredictor
 def model_fn(model_dir):
     args = parse_args()
     args.model_path_prefix = os.path.join(model_dir, 'inference')
-    args.position_prob = 0.5
-    args.max_seq_len = 512
     args.device = 'cpu'
     args.schema = ['法院', {'原告': '委托代理人'}, {'被告': '委托代理人'}]
     predictor = UIEPredictor(args)
@@ -35,8 +33,8 @@ def model_fn(model_dir):
 
 
 def input_fn(request_body, request_content_type):
-    print('[DEBUG] request_body:', type(request_body))
-    print('[DEBUG] request_content_type:', request_content_type)
+#     print('[DEBUG] request_body:', type(request_body))
+#     print('[DEBUG] request_content_type:', request_content_type)
     
     """An input_fn that loads a pickled tensor"""
     if request_content_type == 'application/json':
@@ -48,7 +46,7 @@ def input_fn(request_body, request_content_type):
         return request_body
     
 def predict_fn(input_data, model):
-    outputs = model.predict(texts)
+    outputs = model.predict(input_data)
     return outputs
 
 
@@ -58,68 +56,39 @@ def predict_fn(input_data, model):
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    # Required parameters
+#     # Required parameters
 #     parser.add_argument(
 #         "--model_path_prefix",
 #         type=str,
 #         required=True,
 #         help="The path prefix of inference model to be used.", )
-#     parser.add_argument(
-#         "--position_prob",
-#         default=0.5,
-#         type=float,
-#         help="Probability threshold for start/end index probabiliry.", )
-#     parser.add_argument(
-#         "--max_seq_len",
-#         default=512,
-#         type=int,
-#         help="The maximum input sequence length. Sequences longer than this will be split automatically.",
-#     )
+    parser.add_argument(
+        "--position_prob",
+        default=0.5,
+        type=float,
+        help="Probability threshold for start/end index probabiliry.", )
+    parser.add_argument(
+        "--max_seq_len",
+        default=512,
+        type=int,
+        help="The maximum input sequence length. Sequences longer than this will be split automatically.",
+    )
+    
+    parsed, unknown = parser.parse_known_args() # this is an 'internal' method
+    # which returns 'parsed', the same as what parse_args() would return
+    # and 'unknown', the remainder of that
+    # the difference to parse_args() is that it does not exit when it finds redundant arguments
+
+    for arg in unknown:
+        if arg.startswith(("-", "--")):
+            # you can pass any arguments to add_argument
+            parser.add_argument(arg, type=str)
+
     args = parser.parse_args()
     return args
 
 
-def main():
-    args = parse_args()
-
-    texts = [
-        '"北京市海淀区人民法院\n民事判决书\n(199x)建初字第xxx号\n原告：张三。\n委托代理人李四，北京市 A律师事务所律师。\n被告：B公司，法定代表人王五，开发公司总经理。\n委托代理人赵六，北京市 C律师事务所律师。"',
-        '原告赵六，2022年5月29日生\n委托代理人孙七，深圳市C律师事务所律师。\n被告周八，1990年7月28日出生\n委托代理人吴九，山东D律师事务所律师'
-    ]
-    schema1 = ['法院', {'原告': '委托代理人'}, {'被告': '委托代理人'}]
-    schema2 = [{'原告': ['出生日期', '委托代理人']}, {'被告': ['出生日期', '委托代理人']}]
-
-    args.device = 'cpu'
-    args.schema = schema1
-    predictor = UIEPredictor(args)
-
-    print("-----------------------------")
-    outputs = predictor.predict(texts)
-    for text, output in zip(texts, outputs):
-        print("1. Input text: ")
-        print(text)
-        print("2. Input schema: ")
-        print(schema1)
-        print("3. Result: ")
-        pprint(output)
-        print("-----------------------------")
-
-    # Reset schema
-    predictor.set_schema(schema2)
-    outputs = predictor.predict(texts)
-    for text, output in zip(texts, outputs):
-        print("1. Input text: ")
-        print(text)
-        print("2. Input schema: ")
-        print(schema2)
-        print("3. Result: ")
-        pprint(output)
-        print("-----------------------------")
-
-
 if __name__ == "__main__":
-#     main()
-
     texts = [
         '"北京市海淀区人民法院\n民事判决书\n(199x)建初字第xxx号\n原告：张三。\n委托代理人李四，北京市 A律师事务所律师。\n被告：B公司，法定代表人王五，开发公司总经理。\n委托代理人赵六，北京市 C律师事务所律师。"',
         '原告赵六，2022年5月29日生\n委托代理人孙七，深圳市C律师事务所律师。\n被告周八，1990年7月28日出生\n委托代理人吴九，山东D律师事务所律师'
